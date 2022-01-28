2977da8fc82bfd179289372a57976f5404edfe8a
Sundram <sundramtpvc@gmail.com>
2022-01-28 18:14:05 +0530

    add validation

--- CrudOperation_CodeFirst/Models/Employee.cs
+++ CrudOperation_CodeFirst/Models/Employee.cs
@@ -18,6 +18,7 @@ namespace CrudOperation_CodeFirst.Models
 
         [Required(ErrorMessage = "Employee Name Required!")]
         [MaxLength(80)]
+        [RegularExpression(@"[a-zA-Z][a-zA-Z ]*", ErrorMessage ="Employee Name Not Valid !")]
         public String EmployeeName { get; set; }
 
         [Required(ErrorMessage = "Gmail Required!")]
@@ -26,12 +27,14 @@ namespace CrudOperation_CodeFirst.Models
                             @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
                             ErrorMessage = "Email is not valid")]
         public String EmployeeGmail { get; set; }
-
+       
         [Required(ErrorMessage = "Employee Contact Required!")]
+        [RegularExpression(@"^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$", ErrorMessage = "Employee Contact Not Valid !")]
         [MaxLength(80)]
         public String EmployeeContact { get; set; }
 
         [Required(ErrorMessage = "Employee Adress Required!")]
+        [RegularExpression(@"[a-zA-Z][a-zA-Z ]*", ErrorMessage = "Employee Adress Not Valid !")]
         [MaxLength(80)]
         public String EmployeeAdress { get; set; }
 
